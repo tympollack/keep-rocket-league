b7ec58783d694251f0e71e27e328e86cc469239e
tpollack <tpollack@gaig.com>
2019-06-14 12:01:05 -0400

    'init'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app light>
+    <v-content>
+      <section>
+        <v-parallax :src="require('./assets/rocket-league-banner.jpg')" height="500">
+          <v-layout
+                  column
+                  align-center
+                  justify-center
+          >
+            <h1 class="white--text px-3 py-1 mb-2 display-1 text-xs-center" style="background-color:rgba(0,0,0,0.5);">
+              Sign the Petition and Keep Rocket League on Steam!
+            </h1>
+            <a href="#" style="width:250px">
+              <v-img :src="require('./assets/steam-sign-in-button.png')"></v-img>
+            </a>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+                column
+                wrap
+                class="my-5"
+                align-center
+        >
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">{{ numPlayers }} players have signed!</h2>
+            </div>
+            <table-list></table-list>
+          </v-flex>
+        </v-layout>
+      </section>
+
+      <section>
+        <v-parallax :src="require('./assets/rocket-league-banner-lg.jpg')" height="380">
+
+        </v-parallax>
+      </section>
+
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableList from './components/TableList'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    TableList
+  },
+  data () {
+    return {
+      numPlayers: 69
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
